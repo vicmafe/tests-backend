b4ef77ff822f3c790eecdbfd8026865f29aaf4b1
vicmafe <victormateuslf@gmail.com>
2021-05-20 21:33:01 -0300

    create first test

---
+++ initialTest.test.js
@@ -0,0 +1,7 @@
+const productController = require('./controllers/productController');
+const productModel = require('./models/productModel');
+
+describe('teste inicial para verificar se tudo ok', () => {
+  expect(2+2).toBe(4);
+});
+
